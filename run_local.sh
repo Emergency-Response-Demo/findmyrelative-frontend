1e18d74195cbce340c04b7c1373943359f956ae3
Meghana Kanagal <mkanagal@localhost.localdomain>
2019-12-21 21:39:57 +0530

    New

--- run_local.sh
+++ run_local.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
 export REACT_APP_BACKEND_URL="http://localhost:8080"
-export REACT_APP_MAPBOX_TOKEN="your_token_here"
+export REACT_APP_MAPBOX_TOKEN="pk.eyJ1IjoibWVnaGFuYXNrYW5hZ2FsIiwiYSI6ImNrMXExdmU5ZjEyYWczY3FvZmw2dm9oamwifQ.qDuxo-hNK1XTwOGDRsvSTA"
 
 npm start
