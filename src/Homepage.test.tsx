c7f7b53667a3f3b0faca98ef3755cd640e50846f
Mohammed Saud <msaud@redhat.com>
2020-02-04 11:19:11 +0530

    create mount smoke test for Homepage

--- src/Homepage.test.tsx
+++ src/Homepage.test.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Enzyme from 'enzyme'
 import Homepage from './Homepage'
 
-const { shallow } = Enzyme
+const { mount } = Enzyme
 
 // import Page from '@patternfly/react-core'
 
-it('consists of Page', () => {
-  shallow(<Homepage />)
+it('renders without crashing', () => {
+  mount(<Homepage />)
 })
