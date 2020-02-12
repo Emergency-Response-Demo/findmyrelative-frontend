65ca4c0b5651ea3668f6fceb5e32ceaf31a5c030
Mohammed Saud <msaud@redhat.com>
2020-02-12 12:14:23 +0530

    replace Object.assign with spread operator

--- src/redux/reducers/details.ts
+++ src/redux/reducers/details.ts
@@ -16,20 +16,17 @@ type ActionType = RequestDetailsType & RecieveDetailsType;
 export function detailsReducer (state = initialState, action: ActionType) {
   switch (action.type) {
     case REQUEST_DETAILS:
-      return Object.assign(state, { isFetching: true })
+      return { ...state, isFetching: true }
     case RECIEVE_DETAILS:
       if (action.payload.isSuccessful) {
-        return Object.assign(state, {
+        return {
+          ...state,
           hasData: true,
           isFetching: false,
           data: action.payload.data
-        })
+        }
       } else {
-        return Object.assign(state, {
-          hasData: false,
-          isFetching: false,
-          data: []
-        })
+        return { ...state, hasData: false, isFetching: false, data: [] }
       }
     default:
       return state
